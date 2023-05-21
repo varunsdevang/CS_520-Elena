1ccba908720cce732ce3bdf1fea331771a6fe8cb
KritikaKapoor13 <41050755+KritikaKapoor13@users.noreply.github.com>
2023-05-20 20:54:04 -0400

    Removed backend_algos folder, refactored structure

--- backend_algos/ReadMe.md
+++
@@ -1,1 +0,0 @@
-# REST Backend using Django Framework.
--- backend_algos/model/utils.py
+++
@@ -1,9 +0,0 @@
-def checkForSourceAndDestCity(start,end):
-    source_address = start.split(",")
-    destination_address = end.split(",")
-    source_city,source_state = source_address[-3].strip(),source_address[-2].strip()
-    dest_city,dest_state = destination_address[-3].strip(),destination_address[-2].strip()
-    if(source_city==dest_city and source_state==dest_state):
-        return {"result":True,"city":source_city,"state":source_state}
-    else:
-        return {"result":False}

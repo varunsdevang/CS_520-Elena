162e72ab9ee07008c978f1b2d1d92c67f0f07bc0
KritikaKapoor13 <41050755+KritikaKapoor13@users.noreply.github.com>
2023-05-20 20:45:09 -0400

    Added controller

---
+++ controller.py
@@ -0,0 +1,33 @@
+from flask import Flask,request,jsonify
+from model.graph import Graph
+import utils
+import djikistra
+
+app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/get-route", methods = ['GET'])
+def get_route():
+    starting_point = request.args.get("source")
+    ending_point = request.args.get("destination")
+    mode = "driving"
+    percent_gain = "125"
+    elevation = "max"
+
+    model = Graph(starting_point,ending_point)
+    G = model.get_graph()
+    path_finder = djikistra.Djikistra(G,starting_point,ending_point)
+    nodes_list = path_finder.shortest_path()
+    path_coordinates = utils.convert_nodes_to_coordinates(G,nodes_list)
+    return jsonify({"result":path_coordinates})
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)

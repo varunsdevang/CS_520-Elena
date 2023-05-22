648dce444a3272c92966893dc3e33883ccea5f0b
KritikaKapoor13 <41050755+KritikaKapoor13@users.noreply.github.com>
2023-05-22 13:07:22 -0400

    Added mode specific graph generation

--- model/utils.py
+++ model/utils.py
@@ -1,14 +1,40 @@
-def checkForSourceAndDestCity(start,end):
-    source_address = start.split(",")
-    destination_address = end.split(",")
-    source_city,source_state = source_address[-3].strip(),source_address[-2].strip()
-    dest_city,dest_state = destination_address[-3].strip(),destination_address[-2].strip()
-    if(source_state==dest_state):
-        state_match = True
-        if(source_city==dest_city):
-            city_match = True
+import osmnx as ox
+
+class UtilsForModel:
+
+    def checkForSourceAndDestCity(self,start,end):
+        source_address = start.split(",")
+        destination_address = end.split(",")
+        source_city,source_state = source_address[-3].strip(),source_address[-2].strip()
+        dest_city,dest_state = destination_address[-3].strip(),destination_address[-2].strip()
+        if(source_state==dest_state):
+            state_match = True
+            if(source_city==dest_city):
+                city_match = True
+            else:
+                city_match = False
         else:
-            city_match = False
-    else:
-        state_match = False
-    return {"city_match":city_match, "state_match":state_match, "result":{"city":source_city if city_match else None,"state":source_state if state_match else None}}
+            state_match = False
+        return {"city_match":city_match, "state_match":state_match, "result":{"city":source_city if city_match else None,"state":source_state if state_match else None}}
+
+    def get_path_time(self,path):
+            node_pointer = 1
+            prev_node_pointer = 0
+            travel_time = graph.edges[path[prev_node_pointer],path[node_pointer],0]["travel_time"]
+            while(node_pointer<len(path)-1):
+                prev_node_pointer+=1
+                node_pointer+=1
+                travel_time+=graph.edges[path[prev_node_pointer],path[node_pointer],0]["travel_time"]
+            return travel_time
+
+    def calculateTimeTakenForEachEdge(self,graph,mode):
+        graph = ox.add_edge_speeds(graph)
+        graph = ox.add_edge_travel_times(graph)
+        if mode == "walk":
+            travel_speed = 2.8
+        elif mode == "bike":
+            travel_speed = 10.06
+        if mode != "drive":
+            for u, v, k, data in graph.edges(data=True, keys=True):
+                data['travel_time'] = data['length'] / travel_speed
+        return graph
